9a9f9285288869f31a4179919271bfa68355173e
Djumali Osmani <xhumaliosmani@outlook.com>
2023-06-04 20:25:21 +0200

    first commit

--- app/about/page.js
+++ app/about/page.js
@@ -5,7 +5,7 @@ import Footer from "@/src/Footer/Footer";
 import Navbar from "@/src/Navbar/Navbar";
 import TeamImage from "../../public/images/Team/team1.png";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const About = ({props}) => {
     const ref = useRef(null);
